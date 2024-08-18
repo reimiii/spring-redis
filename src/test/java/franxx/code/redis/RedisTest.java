b07e174016dcfce48ade6ff34c8a35fcdfb4c3b9
Hilmi AM <imiia75775@proton.me>
2024-08-18 14:18:34 +0700

    test: value operation

--- src/test/java/franxx/code/redis/RedisTest.java
+++ src/test/java/franxx/code/redis/RedisTest.java
@@ -2,10 +2,13 @@ package franxx.code.redis;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class RedisTest {
@@ -16,6 +19,16 @@ class RedisTest {
     @Test
     void redisTemplate() {
        assertThat(redisTemplate).isNotNull();
+    }
+
+    @Test
+    void strings() throws InterruptedException {
+       ValueOperations<String,String> opsForValue = redisTemplate.opsForValue(); 
+       opsForValue.set("name", "mee", Duration.ofSeconds(2));
+
+       assertThat("mee").isEqualTo(opsForValue.get("name"));
+       Thread.sleep(Duration.ofSeconds(3));
+       assertThat(opsForValue.get("name")).isNull();
     }
 
 
